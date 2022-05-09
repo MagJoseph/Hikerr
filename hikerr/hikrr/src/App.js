61e5b38c0b231300a9c74c527f1d8f4c7b0550c8
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-05-09 16:05:05 -0400

    added checkToken function to App.js

--- hikerr/hikrr/src/App.js
+++ hikerr/hikrr/src/App.js
@@ -2,12 +2,30 @@ import Posts from './components/Posts';
 import Home from './pages/Home';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { CheckSession } from "./services/Auth";
 import PostDetails from './components/PostDetails';
 import MyHikes from './pages/MyHikes';
 import Navbar from './components/Navbar';
 import Search from './components/Search';
+import CreateComment from './pages/CreateComment';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState('')
+
+  let navigate = useNavigate();
+  
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+    toggleAuthenticated(true)
+}
+
+
   return (
     <div className="App">
       <Navbar />
@@ -18,6 +36,7 @@ function App() {
           <Route path="/posts/postdetail/:id" element={<PostDetails />} />
           <Route path="/myhikes" element={<MyHikes />} />
           <Route path="/search" element={<Search />} />
+          <Route path="/createcomment" element={<CreateComment />} />
         </Routes>
       </main>
     </div>
