daddd39ec91883818c4320b08da5af41c55d5a4c
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-05-09 15:47:18 -0400

    created SignInUser function

--- hikerr/hikrr/src/services/Auth.js
+++ hikerr/hikrr/src/services/Auth.js
@@ -1,1 +1,13 @@
+import Client from "./api";
 
+
+// Set the current signed in users token to localStorage
+export const SignInUser = async (data) => {
+  try {
+    const res = await Client.post("login", data);
+   localStorage.setItem("token", res.data.token);
+    return res.data.user;
+  } catch (error) {
+    throw error;
+  }
+};
