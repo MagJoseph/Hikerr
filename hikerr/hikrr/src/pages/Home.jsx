5e87bb42c41bd773030708f150340b85255a0d6b
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-05-09 11:59:11 -0400

    added links to home page

--- hikerr/hikrr/src/pages/Home.jsx
+++ hikerr/hikrr/src/pages/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div className="home-container">
+        HIKRR
+        <div className="home-title">Where your adventure begins</div>
+      </div>
+      <div className="link-wrapper centered">
+        <Link to="/posts" className="item">Hiking Trails</Link>
+        <Link to="/register" className="item1">Register</Link>
+        <Link to="/login" className="item1">Login</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home
