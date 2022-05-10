4f00fdfb60836ca5eedec23591351a94e5607478
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-05-10 17:38:48 -0400

    added styling to home.js

--- hikerr/hikrr/src/pages/Home.jsx
+++ hikerr/hikrr/src/pages/Home.jsx
@@ -3,15 +3,30 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <div className="home-container">
-        HIKRR
-        <div className="home-title">Where your adventure begins</div>
+        <span>H</span>
+        <span>I</span>
+        <span>K</span>
+        <span>R</span>
+        <span>R</span>
+        <div className="title">Where your adventure begins</div>
       </div>
       <div className="link-wrapper centered">
         {/* <Link to="/posts" className="item">Hiking Trails</Link> */}
-        <Link to="/register" className="item1">Register</Link>
-        <Link to="/login" className="item1">Login</Link>
+        <br></br>
+        <div className="land-container">
+          <div className="landing">
+            <Link to="/register" className="link">
+              Register
+            </Link>
+          </div>
+          <div className="landing">
+            <Link className="link" to="/login">
+              Log in
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
