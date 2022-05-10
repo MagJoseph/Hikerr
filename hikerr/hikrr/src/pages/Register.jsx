202a928c5932f19c42fac2de607d208d73dd334d
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-05-10 09:45:57 -0400

    created register component

--- hikerr/hikrr/src/pages/Register.jsx
+++ hikerr/hikrr/src/pages/Register.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
+import { useEffect, useState } from "react";
+import { RegisterUser } from "../services/Auth";
+import { useNavigate } from "react-router-dom";
+import Client from "../services/api";
 
 const Register = () => {
+
+  let navigate = useNavigate()
+
+  //set form values
+  const [formValues, setFormValues] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+})
+
+  const [usernameList, setUsernameList] = useState()
+  const [emailList, setEmailList] = useState()
+  const [usedInfo, setUsedInfo] = usedState(true)
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      let usernameArr = []
+      let emailArr = []
+    }
+  })
+  
   return (
     <div>Register</div>
   )
