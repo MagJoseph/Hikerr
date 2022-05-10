d2230dc999400bf93c4e557be32cd7aa532fcce8
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-05-10 14:34:45 -0400

    set up search component

--- hikerr/hikrr/src/components/Search.jsx
+++ hikerr/hikrr/src/components/Search.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import Client from "../services/api";
+import { useState } from "react";
 
 
 const Search = () => {
+
+   const [searchResults, setSearchResults] = useState([]);
+   const [searchQuery, setSearchQuery] = useState("");
+
+
   return (
     <div>Search</div>
   )
