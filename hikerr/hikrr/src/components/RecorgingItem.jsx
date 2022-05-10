1b033860159acda2e322ee2f81032bbe1a8400b3
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-05-10 14:24:16 -0400

    created axios call to post times

--- hikerr/hikrr/src/components/RecorgingItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RecorgingItem = () => {
-  return (
-    <div>RecorgingItem</div>
-  )
-}
-
-export default RecorgingItem
--- hikerr/hikrr/src/pages/YourRecordings.jsx
+++ hikerr/hikrr/src/pages/YourRecordings.jsx
@@ -1,24 +1,60 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import Client from "../services/api";
+import RecordingItem from '../components/RecordingItem';
+import { useNavigate } from 'react-router-dom'
 
 const YourRecordings = () => {
 
-  const [time, setTime] = useState([])
+  let navigate = useNavigate();
+
+  const [times, setTime] = useState([])
   
   const getTimes = async () => {
-    const times = await Client.get(`http://localhost:3001/times`);
-    console.log(times.data)
-    setTime(times.data)
+    const recordings = await Client.get(`http://localhost:3001/times`);
+    console.log(recordings.data)
+    setTime(recordings.data)
   }
  
   useEffect(() => {
   getTimes()
-}, [])
+  }, [])
+  
+  const [newRecording, setRecording] = useState({
+    name: '',
+    content: '',
+    distance: ''
+  })
+  
+  const getNewTiming = async () => {
+    await Client({
+      url: `http://localhost:3001/times`,
+      method: 'post',
+      data: newRecording
+    });
+  }
+
+    const handleChange = (e) => {
+      setRecording({ ...newRecording, [e.target.name]: e.target.value });
+      console.log(e.target.name);
+      console.log(e.target.value);
+    };
+
+
+
 
   return (
     <div>
       <h1>Your Recordings:</h1>
+      {times.map((time) => (
+        <div className="times-container" key={time.id}>
+          <RecordingItem
+            name={time.name}
+            content={time.content}
+            distance={time.distance}
+          />
+        </div>
+      ))}
     </div>
   );
 }
