ebd024f899ce8b0d19c96e329457464aa2b301c0
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-05-12 14:29:58 -0400

    created weatherComponent

---
+++ hikerr/hikrr/src/components/WeatherComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+import Client from "../services/api";
+
+const WeatherComponent = () => {
+
+
+
+
+  return (
+    <div>WeatherComponent</div>
+  )
+}
+
+export default WeatherComponent
